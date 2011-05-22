0ea221d76f6bfdec4b6af9a6a5cbb1963e02c828
Brian Gupta <brian.gupta@brandorr.com>
2011-05-22 09:23:05 -0400

    Added LICENSE and added info to README

--- dnsmeapi.rb
+++ dnsmeapi.rb
@@ -1,6 +1,20 @@
 #!/opt/local/bin/ruby
-# Copyright Vijay Brian Gupta 2011 brian.gupta@brandorr.com
-# License GPLv2 http://www.gnu.org/licenses/gpl-2.0.html (replace with full text)
+# Copyright (C) 2011 Vijay Brian Gupta brian.gupta@brandorr.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 # v0.2 May 22, 2011 - Replicated functionality of perl script http://www.dnsmadeeasy.com/enterprisedns/api.html
 #
 # Make sure the dnsmeapi.properties file is available in the location from which you run this
@@ -8,7 +22,6 @@
 require 'time'
 require 'uri'
 require 'openssl'
-require 'yaml'
 
 dmepropertyfile = "dnsmeapi.properties"
 requestDate = Time.now.httpdate
