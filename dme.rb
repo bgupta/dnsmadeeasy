9630d4198d24378620cd159d30d0f965d755f597
Brian Gupta <brian.gupta@brandorr.com>
2011-05-24 19:04:31 -0400

    Added dme.rb, which are my initial attempts to work with native ruby rest-client rather than shelling out to curl.

---
+++ dme.rb
@@ -0,0 +1,69 @@
+#!/opt/local/bin/ruby
+# Copyright (C) 2011 Vijay Brian Gupta brian.gupta@brandorr.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# v0.2 May 22, 2011 - Replicated functionality of perl script http://www.dnsmadeeasy.com/enterprisedns/api.html
+# v0.3 May 24, 2011 - Replaced sheeling out to curl with native ruby rest-client call
+
+# Make sure the dnsmeapi.properties file is available in the location from which you run this
+
+require 'time'
+require 'uri'
+require 'openssl'
+require 'rest_client'
+require 'json'
+require 'set'
+
+dmepropertyfile = "dnsmeapi.properties"
+requestDate = Time.now.httpdate
+
+ def load_properties(propertyfile)
+    properties = {}
+    File.open(propertyfile, 'r') do |propertyfile|
+      propertyfile.read.each_line do |line|
+        line.strip!
+        if (line[0] != ?# and line[0] != ?=)
+          i = line.index('=')
+          if (i)
+            properties[line[0..i - 1].strip] = line[i + 1..-1].strip
+          else
+            properties[line] = ''
+          end
+        end
+      end
+    end
+    properties
+  end
+
+prophash = load_properties dmepropertyfile
+apiKey = prophash["apiKey"]
+secretKey = prophash["secretKey"]
+
+hmac = OpenSSL::HMAC.hexdigest('sha1', secretKey, requestDate)
+
+apiKeyHeader = "x-dnsme-apiKey:" + apiKey;
+hmacHeader = "x-dnsme-hmac:" + hmac;
+requestDateHeader = "x-dnsme-requestDate:" + "\"" + requestDate + "\"";
+
+response = RestClient.get 'http://api.dnsmadeeasy.com/V1.2/domains/brandorr.com/records', :"x-dnsme-apiKey" => apiKey, :"x-dnsme-hmac" => hmac, :"x-dnsme-requestDate" => requestDate, :accept =>:json
+results = JSON.parse(response.to_str)
+#puts JSON.pretty_generate(results)
+#puts results[1]["name"]
+#puts JSON.pretty_generate(results.select {|x| x["name"] == "www"})
+#puts JSON.pretty_generate(results.select {|x| x["name"] == "www" and x["type"] == "A"})
+nameresults = results.select {|x| x["name"] == "www" and x["type"] == "A"}
+#puts nameresults
+nameresults.each {|x| puts x["id"]}
